ccab8a5b0e57315f015ca4141bb2c8df4961ef58
ammo414 <anmol.eswarapu@protonmail.com>
2024-01-01 23:20:40 -0600

    tabs to spaces

--- graphqlqueries.py
+++ graphqlqueries.py
@@ -1,44 +1,44 @@
 queryAnimeLists = '''
-	query ($username: String){
-		MediaListCollection (userName: $username, type: ANIME) {
-			lists {
-				status
-				entries {
-					media {
-						meanScore
-					}
-					score(format: POINT_100)
-					mediaId
-				}
-			}
-		}
-	}
-	'''
+    query ($username: String){
+        MediaListCollection (userName: $username, type: ANIME) {
+            lists {
+                status
+                entries {
+                    media {
+                        meanScore
+                    }
+                    score(format: POINT_100)
+                    mediaId
+                }
+            }
+        }
+    }
+    '''
 
 queryRecsForAnime = '''
-	query ($id: Int) {
-		Media (id: $id) {
-			id
-			title {
-				english
-				romaji
-			}
-			recommendations(sort: RATING_DESC, page: 1, perPage: 10){
-				pageInfo{
-					perPage
-					hasNextPage
-				}
-				nodes {
-					mediaRecommendation {
-						id
-						title {
-							english
-							romaji
-						}
-						meanScore
-					}
-				}
-			}
-		}
-	}
-	'''
+    query ($id: Int) {
+        Media (id: $id) {
+            id
+            title {
+                english
+                romaji
+            }
+            recommendations(sort: RATING_DESC, page: 1, perPage: 10){
+                pageInfo{
+                    perPage
+                    hasNextPage
+                }
+                nodes {
+                    mediaRecommendation {
+                        id
+                        title {
+                            english
+                            romaji
+                        }
+                        meanScore
+                    }
+                }
+            }
+        }
+    }
+    '''
